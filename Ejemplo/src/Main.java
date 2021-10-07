ad3291b7d754489ac409c2156968694dae142014
yanis <yanis@10.164.51.137>
2021-10-07 16:36:57 +0200

    int e string

--- Ejemplo/src/Main.java
+++ Ejemplo/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hola Mundo!");
+			
+			String seggso = "Me caguen";
+			int i = 0;
 	}
 
 }
